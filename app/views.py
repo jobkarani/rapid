20fd8a00f96cdf1644290f8ae96f857cb9eb4a47
Ayub254 <jobkarani084j@gmail.com>
2022-06-27 09:06:29 +0300

    creates blogdetails view

--- app/views.py
+++ app/views.py
@@ -20,3 +20,21 @@ def blogs(request):
         "form":form,
     }
     return render (request, 'blog.html', ctx )
+
+def blogDetails(request,blogs_id):
+    posts = Blogs.objects.all()
+    form = ReplyForm()
+    blog = Blogs.objects.filter(pk = blogs_id)
+    if request.method == 'POST':  
+        form = ReplyForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('blogs')
+    print(posts)
+    ctx= {
+        "posts":posts,
+        "form":form,
+        "blog":blog,
+    }
+    return render (request, 'blog-details.html', ctx)
