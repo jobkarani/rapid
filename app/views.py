5df6082fab59102198b860f7b7b8143b988f79bc
Ayub254 <jobkarani084j@gmail.com>
2022-06-27 09:05:48 +0300

    creates blog view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from app.forms import CommentForm,ReplyForm
+from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def blogs(request):
+    posts = Blogs.objects.all()
+    form = ReplyForm()
+    if request.method == 'POST':  
+        form = ReplyForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('blogs')
+    print(posts)
+    ctx= {
+        "posts":posts,
+        "form":form,
+    }
+    return render (request, 'blog.html', ctx )
