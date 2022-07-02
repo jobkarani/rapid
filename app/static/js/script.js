7df5b3665503adddfaf88cd51203802c2f6e03c5
derrick macharia <softwaredevderrick@gmail.com>
2022-07-02 03:09:48 -0700

    fix swiper

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menus");
+const navLink = document.querySelectorAll(".nav-links");
+
+const mobileMenu = () => {
+  hamburger.classList.toggle("active")
+  navMenu.classList.toggle("active")
+}
+
+const closeMenu = () => {
+  hamburger.classList.remove("active")
+  navMenu.classList.remove("active")
+}
+
+hamburger.addEventListener("click" , mobileMenu)
+navLink.forEach((l) => l.addEventListener("click",closeMenu))
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 20,
@@ -6,20 +23,3 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
-
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menus");
-  const navLink = document.querySelectorAll(".nav-links");
-  
-  const mobileMenu = () => {
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-  }
-  
-  const closeMenu = () => {
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-  }
-  
-  hamburger.addEventListener("click" , mobileMenu)
-  navLink.forEach((l) => l.addEventListener("click",closeMenu))
