68aef9bb6b78f82854e8bc309d5bc9c5010981fa
derrick macharia <softwaredevderrick@gmail.com>
2022-07-02 05:21:42 -0700

    recreate carousel

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -15,13 +15,25 @@ const closeMenu = () => {
 hamburger.addEventListener("click" , mobileMenu)
 navLink.forEach((l) => l.addEventListener("click",closeMenu))
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
     },
-  });
-
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
  
