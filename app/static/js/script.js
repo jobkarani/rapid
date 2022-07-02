88c3f9c84a85a9747f3f6da4750cf17ecb48eb10
derrick macharia <softwaredevderrick@gmail.com>
2022-07-02 01:52:34 -0700

    style navbar

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -6,3 +6,20 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menus");
+  const navLink = document.querySelectorAll(".nav-links");
+  
+  const mobileMenu = () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+  }
+  
+  const closeMenu = () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+  }
+  
+  hamburger.addEventListener("click" , mobileMenu)
+  navLink.forEach((l) => l.addEventListener("click",closeMenu))
